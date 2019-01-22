d7154d43c5df38ea0c8c55ce34b4f8dd10d143a6
klcvf <glloqwow0784015@gmail.com>
2019-01-22 19:01:27 +0800

    123

---
+++ src/main/java/com/apple/Hello.java
@@ -0,0 +1,9 @@
+package com.apple;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+        System.out.println("Hello word123");
+
+    }
+}
---
+++ src/main/kotlin/com/apple/Hello.kt
@@ -0,0 +1,6 @@
+package com.apple
+
+fun main(args: Array<String>) {
+    println("Hello kotlin")
+    println("Hello kotlin")
+}
